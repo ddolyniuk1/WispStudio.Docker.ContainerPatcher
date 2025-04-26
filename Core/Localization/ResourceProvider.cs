1ab5b89787c6aa2a91d28b1e520617fbb621a752
ddolyniuk1 <ddolyniuk+github@gmail.com>
2025-04-26 18:50:31 -0400

    Update ResourceProvider.cs

--- Core/Localization/ResourceProvider.cs
+++ Core/Localization/ResourceProvider.cs
@@ -1,4 +1,6 @@
 ﻿using System.Globalization;
+using System.Reflection;
+using System.Resources;
 
 namespace WispStudios.Docker.ContainerPatcher.Core.Localization
 {
@@ -7,15 +9,35 @@ namespace WispStudios.Docker.ContainerPatcher.Core.Localization
         private static CultureInfo _currentCulture;
 
         static ResourceProvider()
-        { 
-            _currentCulture = CultureInfo.CurrentUICulture;
+        {
+            StringResourceManager = new ResourceManager(
+                "WispStudios.Docker.ContainerPatcher.Core.Resources.Strings",
+                Assembly.GetExecutingAssembly());
+            ErrorResourceManager = new ResourceManager(
+                "WispStudios.Docker.ContainerPatcher.Core.Resources.Errors",
+                Assembly.GetExecutingAssembly());
+            _currentCulture = CultureInfo.CurrentUICulture; 
         }
+
+        public static ResourceManager ErrorResourceManager { get; set; }
+
+        public static ResourceManager StringResourceManager { get; set; }
 
         public static void SetCulture(string cultureName)
         {
             _currentCulture = new CultureInfo(cultureName);
             CultureInfo.CurrentUICulture = _currentCulture;
             CultureInfo.CurrentCulture = _currentCulture;
+        }
+
+        public static string? GetString(string resourceKey)
+        {
+            return StringResourceManager.GetString(resourceKey);
+        }
+
+        public static string? GetError(string resourceKey)
+        {
+            return ErrorResourceManager.GetString(resourceKey);
         }
     }
 }
